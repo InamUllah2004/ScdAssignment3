11f78cce946cc56c8fed7514d254e149f5cf4289
Inam Ullah <inamu0448@gmail.com>
2025-10-31 16:49:04 +0500

    first commit

---
+++ heavens.test.js
@@ -0,0 +1,23 @@
+/* eslint-env jest */
+const utils = require('./src/utils');
+const satellite = require('./src/satellite');
+const iridium = require('./src/iridium');
+
+describe('Basic CI pipeline tests', () => {
+  test('utils.getTimestamp returns expected seconds for hh:mm:ss', () => {
+    expect(utils.getTimestamp('01:02:03')).toBe(3723);
+  });
+
+  test('utils.md5 returns correct md5 length', () => {
+    const res = utils.md5('test');
+    expect(res).toHaveLength(32);
+  });
+
+  test('satellite exports getTable function', () => {
+    expect(typeof satellite.getTable).toBe('function');
+  });
+
+  test('iridium exports getTable function', () => {
+    expect(typeof iridium.getTable).toBe('function');
+  });
+});
